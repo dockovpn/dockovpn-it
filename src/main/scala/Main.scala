d638edbd429c369894b53851555ec044aa3225cb
Alexander Litvinenko <alekslitvinenk@gmail.com>
2020-08-04 00:17:43 +0300

    Dockovpn image

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,30 +1,11 @@
 
-import java.time._
-
-import com.dimafeng.testcontainers.MySQLContainer
-import org.testcontainers.containers.wait.strategy.Wait
-import org.testcontainers.containers.{FixedHostPortGenericContainer, InternetProtocol}
+import com.alekslitvinenk.testcontainers.DockovpnContainer
 
 import scala.concurrent.{Await, Future}
 
 object Main extends App {
   
-  private val httpPort = 8080
-  private val udpPort = 1194
-  private val dockovpn = "alekslitvinenk/openvpn"
-  private val logEntryRegEx = ".*Config\\sserver\\sstarted.*"
-  private val startupTimeout = Duration.ofSeconds(10)
-  private val container = new FixedHostPortGenericContainer(dockovpn)
-  container.withFixedExposedPort(httpPort, httpPort, InternetProtocol.TCP)
-  container.withFixedExposedPort(udpPort, udpPort, InternetProtocol.UDP)
-  container.withPrivilegedMode(true)
-  container.waitingFor(
-    Wait.forLogMessage(logEntryRegEx, 1)
-      .withStartupTimeout(startupTimeout)
-  )
-  container.start()
-  
-  val mySQLContainer = MySQLContainer()
+  DockovpnContainer().start()
   
   import scala.concurrent.duration._
   Await.result(Future.never, Duration.Inf)
--- src/main/scala/com/alekslitvinenk/testcontainers/DockovpnContainer.scala
+++ src/main/scala/com/alekslitvinenk/testcontainers/DockovpnContainer.scala
@@ -1,5 +1,34 @@
 package com.alekslitvinenk.testcontainers
 
-class DockovpnContainer {
+import java.time.Duration
+
+import org.testcontainers.containers.{FixedHostPortGenericContainer, InternetProtocol}
+import org.testcontainers.containers.wait.strategy.Wait
+
+object DockovpnContainer {
+  val dockOvpnImageName = "alekslitvinenk/openvpn"
   
+  object Tags {
+    val latest = "latest"
+  }
+  
+  def apply(tag: String = Tags.latest): DockovpnContainer = new DockovpnContainer(tag)
+}
+
+import DockovpnContainer._
+class DockovpnContainer(tag: String)
+  extends FixedHostPortGenericContainer[DockovpnContainer](dockOvpnImageName + ":" + tag) {
+  
+  private val httpPort = 8080
+  private val udpPort = 1194
+  private val logEntryRegEx = ".*Config\\sserver\\sstarted.*"
+  private val startupTimeout = Duration.ofSeconds(10)
+  
+  addFixedExposedPort(httpPort, httpPort, InternetProtocol.TCP)
+  addFixedExposedPort(udpPort, udpPort, InternetProtocol.UDP)
+  withPrivilegedMode(true)
+  waitingFor(
+    Wait.forLogMessage(logEntryRegEx, 1)
+      .withStartupTimeout(startupTimeout)
+  )
 }
