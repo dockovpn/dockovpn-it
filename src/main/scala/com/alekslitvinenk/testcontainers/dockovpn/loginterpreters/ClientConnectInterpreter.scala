81754bd6c55346ba252cd86b11fdb4ef8fbde6ee
Alexander Litvinenko <alekslitvinenk@gmail.com>
2021-10-21 23:28:36 +0300

    Fix ClientConnectInterpreter dependency on date format (date froup removed from regex)

--- src/main/scala/com/alekslitvinenk/testcontainers/dockovpn/loginterpreters/ClientConnectInterpreter.scala
+++ src/main/scala/com/alekslitvinenk/testcontainers/dockovpn/loginterpreters/ClientConnectInterpreter.scala
@@ -16,18 +16,16 @@ class ClientConnectInterpreter(callback: ClientInfoCallback) extends AbstractInt
   override val patternToDetectSequence: Regex = ".*Peer\\sConnection\\sInitiated\\swith.*".r
   
   override protected def parse(): ClientInfo = {
-    val dateRx = "[A-Za-z]{3}\\s[A-Za-z]{3}\\s\\d{1,2}\\s\\d{2}:\\d{2}:\\d{2}\\s\\d{4}"
-    val clientIdentifierRx = "[A-Za-z0-9]+"
+    val clientIdentifierRx = "[A-Za-z0-9]{32}"
     val ipAddressRx = "\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}"
     val localPortRx = "\\d{4,6}"
     val regExp: Regex =
-      s"^($dateRx)\\s($clientIdentifierRx)/($ipAddressRx):($localPortRx)\\s.*IPv4=($ipAddressRx).*".r
+      s"^.*($clientIdentifierRx)/($ipAddressRx):($localPortRx)\\s.*IPv4=($ipAddressRx).*".r
   
-    val regExp(date, clientIdentifier, localIpAddress, localPort, vpnIpAddress) =
+    val regExp(clientIdentifier, localIpAddress, localPort, vpnIpAddress) =
       logs.last.stripLineEnd
     
     ClientInfo(
-      // FixMe: use date from parsed date string
       timeConnected = Date.from(Instant.now()),
       identifier = clientIdentifier,
       localIpAddress = localIpAddress,
